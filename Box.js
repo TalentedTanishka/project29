b782ce756b68de12a71c81189b9b335b1ba4e9e3
TalentedTanishka <67682178+TalentedTanishka@users.noreply.github.com>
2020-08-29 16:52:00 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,34 @@
+class Box 
+{
+  constructor(x, y, width, height) {
+    var options = { 
+      'isStatic' : false,
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display(){
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    strokeWeight(2);
+    fill(0,188,257);
+    rectMode(CENTER);
+    rect( 0, 0, this.width, this.height);
+    pop();
+  }
+  }
+  
+
+
+
+
+  
+    
+  
+
+
